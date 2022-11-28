db18173200f722eac023b5982032bf225e8dbe65
Lexx-Dev <alexcalle.paola@gmail.com>
2022-11-27 20:07:21 -0500

    change font-family

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import viteCompression from 'vite-plugin-compression';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  viteCompression({ algorithm: 'brotliCompress' })],
-});
+  plugins: [
+    react(),
+    viteCompression({ algorithm: 'brotliCompress' }),
+  ],
+})
